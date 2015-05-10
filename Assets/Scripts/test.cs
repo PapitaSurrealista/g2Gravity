89d287762e7e7a9e7a04347b4e5c1ef9a3cebe16
Carlos David Nexans <carlmycol@gmail.com>
2015-05-10 03:25:15 -0430

    Test commit script

--- Assets/Scripts/test.cs
+++ Assets/Scripts/test.cs
@@ -2,12 +2,12 @@
 using System.Collections;
 
 public class test : MonoBehaviour {
-
+	// testtttt
 	// Use this for initialization
 	void Start () {
 	
 	}
-	
+	//testtttt
 	// Update is called once per frame
 	void Update () {
 	
